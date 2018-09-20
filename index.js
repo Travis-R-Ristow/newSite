802738f054f8fdf52336d2e25f73bb32cd5ddcde
unknown <trthegreat19@gmail.com>
2018-09-19 19:44:25 -0700

    Added PopUps for Skills @MV

--- index.js
+++ index.js
@@ -1,13 +1,115 @@
 $( document ).ready(function() {
-	var skillNum = document.getElementsByClassName('gauge').length;
-	console.log(skillNum);
-	for(var i=0; i < skillNum; i++){
-	    $(".gauge:eq("+i+")").mouseenter(function(){
-		    console.log('Show!');
-		    	$('.popUp:eq('+i+')').css('display', 'inline-block');
-		}).mouseleave(function(){
-		    console.log('Hide!');
-		    	$('.popUp:eq('+i+')').css('display', 'none');
-		});
-	}
+    $(".html, .html .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.html').position();
+	    	$('.html .popUp').css('display', 'inline-block');
+	    	$('.html .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.html .popUp').css('display', 'none');
+	});
+
+
+	$(".ruby, .ruby .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.ruby').position();
+	    	$('.ruby .popUp').css('display', 'inline-block');
+	    	$('.ruby .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.ruby .popUp').css('display', 'none');
+	});
+
+
+	$(".js, .js .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.js').position();
+	    	$('.js .popUp').css('display', 'inline-block');
+	    	$('.js .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.js .popUp').css('display', 'none');
+	});
+
+
+	$(".java, .java .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.java').position();
+	    	$('.java .popUp').css('display', 'inline-block');
+	    	$('.java .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.java .popUp').css('display', 'none');
+	});
+
+
+	$(".py, .py .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.py').position();
+	    	$('.py .popUp').css('display', 'inline-block');
+	    	$('.py .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.py .popUp').css('display', 'none');
+	});
+
+
+	$(".ps, .ps .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.ps').position();
+	    	$('.ps .popUp').css('display', 'inline-block');
+	    	$('.ps .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.ps .popUp').css('display', 'none');
+	});
+
+
+	$(".ui, .ui .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.ui').position();
+	    	$('.ui .popUp').css('display', 'inline-block');
+	    	$('.ui .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.ui .popUp').css('display', 'none');
+	});
+
+
+	$(".hw, .hw .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.hw').position();
+	    	$('.hw .popUp').css('display', 'inline-block');
+	    	$('.hw .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.hw .popUp').css('display', 'none');
+	});
+
+
+	$(".c, .c .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.c').position();
+	    	$('.c .popUp').css('display', 'inline-block');
+	    	$('.c .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.c .popUp').css('display', 'none');
+	});
+
+
+	$(".cc, .cc .popUp").mouseenter(function(){
+	    // console.log('Show!');
+	    var pos = $('.cc').position();
+	    	$('.cc .popUp').css('display', 'inline-block');
+	    	$('.cc .popUp').css('top', pos.top,  'left', pos.left);
+	}).mouseleave(function(){
+	    // console.log('Hide!');
+	    	$('.cc .popUp').css('display', 'none');
+	});
+
+
+	
 });
+
+// .html, .java, .py, .ruby, .js, .ps, .ui, .hw, .c, .cc
